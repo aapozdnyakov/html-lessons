b1f0628bb92b2232faa27badee2683ad69e8ed1d
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-10-15 12:11:39 +0300

    модалки

--- SiteModel/index.js
+++ SiteModel/index.js
@@ -1,78 +1,78 @@
 let participants = {
-    actors: [
-        {
-            img: '/assets/CarryCoon.png',
-            name: 'Кэрри Кун',
-            enName: 'Carrie Coon',
-            bio: 'https://ru.wikipedia.org/wiki/Кун,_Кэрри'
-        },
-        {
-            img: '/assets/CarryCoon.png',
-            name: 'Кэрри Кун',
-            enName: 'Carrie Coon',
-            bio: 'https://ru.wikipedia.org/wiki/Кун,_Кэрри'
-        },
-        {
-            img: '/assets/CarryCoon.png',
-            name: 'Кэрри Кун',
-            enName: 'Carrie Coon',
-            bio: 'https://ru.wikipedia.org/wiki/Кун,_Кэрри'
-        },
-        {
-            img: '/assets/CarryCoon.png',
-            name: 'Кэрри Кун',
-            enName: 'Carrie Coon',
-            bio: 'https://ru.wikipedia.org/wiki/Кун,_Кэрри'
-        },
-        {
-            img: '/assets/CarryCoon.png',
-            name: 'Кэрри Кун',
-            enName: 'Carrie Coon',
-            bio: 'https://ru.wikipedia.org/wiki/Кун,_Кэрри'
-        }
-    ],
-    creators: [
-        {
-            img: '/assets/Bill Murray.png',
-            name: 'Билл Мюррей',
-            enName: 'Bill Murray',
-            bio: 'https://ru.wikipedia.org/wiki/Мюррей,_Билл'
-        },
-        {
-            img: '/assets/Bill Murray.png',
-            name: 'Билл Мюррей',
-            enName: 'Bill Murray',
-            bio: 'https://ru.wikipedia.org/wiki/Мюррей,_Билл'
-        },
-        {
-            img: '/assets/Bill Murray.png',
-            name: 'Билл Мюррей',
-            enName: 'Bill Murray',
-            bio: 'https://ru.wikipedia.org/wiki/Мюррей,_Билл'
-        },
-        {
-            img: '/assets/Bill Murray.png',
-            name: 'Билл Мюррей',
-            enName: 'Bill Murray',
-            bio: 'https://ru.wikipedia.org/wiki/Мюррей,_Билл'
-        },
-        {
-            img: '/assets/Bill Murray.png',
-            name: 'Билл Мюррей',
-            enName: 'Bill Murray',
-            bio: 'https://ru.wikipedia.org/wiki/Мюррей,_Билл'
-        }
-    ]
-}
+  actors: [
+    {
+      img: "/assets/CarryCoon.png",
+      name: "Кэрри Кун",
+      enName: "Carrie Coon",
+      bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+    },
+    {
+      img: "/assets/CarryCoon.png",
+      name: "Кэрри Кун",
+      enName: "Carrie Coon",
+      bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+    },
+    {
+      img: "/assets/CarryCoon.png",
+      name: "Кэрри Кун",
+      enName: "Carrie Coon",
+      bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+    },
+    {
+      img: "/assets/CarryCoon.png",
+      name: "Кэрри Кун",
+      enName: "Carrie Coon",
+      bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+    },
+    {
+      img: "/assets/CarryCoon.png",
+      name: "Кэрри Кун",
+      enName: "Carrie Coon",
+      bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+    },
+  ],
+  creators: [
+    {
+      img: "/assets/Bill Murray.png",
+      name: "Билл Мюррей",
+      enName: "Bill Murray",
+      bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+    },
+    {
+      img: "/assets/Bill Murray.png",
+      name: "Билл Мюррей",
+      enName: "Bill Murray",
+      bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+    },
+    {
+      img: "/assets/Bill Murray.png",
+      name: "Билл Мюррей",
+      enName: "Bill Murray",
+      bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+    },
+    {
+      img: "/assets/Bill Murray.png",
+      name: "Билл Мюррей",
+      enName: "Bill Murray",
+      bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+    },
+    {
+      img: "/assets/Bill Murray.png",
+      name: "Билл Мюррей",
+      enName: "Bill Murray",
+      bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+    },
+  ],
+};
 
-showParticipants(participants.actors)
+showParticipants(participants.actors);
 
 function showParticipants(items) {
-    let castBlock = document.querySelector('.cast');
-    castBlock.innerHTML = '';
-    items.forEach((actor) => {
-        let div = document.createElement('div')
-        div.innerHTML = `<a href="${actor.bio}" target="_blank" class="col-3 d-flex mb-4">
+  let castBlock = document.querySelector(".cast");
+  castBlock.innerHTML = "";
+  items.forEach((actor) => {
+    let div = document.createElement("div");
+    div.innerHTML = `<a href="${actor.bio}" target="_blank" class="col-3 d-flex mb-4">
         <img src="${actor.img}">
         <div class="actor d-flex align-items-center">
             <div>
@@ -80,24 +80,26 @@ function showParticipants(items) {
                 <p class="m-0">${actor.enName}</p>
             </div>
         </div>
-    </a>`
-        castBlock.append(div.firstElementChild);
-    })
+    </a>`;
+    castBlock.append(div.firstElementChild);
+  });
 }
-let bottomBlock = document.querySelector('.bottom');
-bottomBlock.addEventListener('click', (event) => {
-    let button = event.target.closest('[data-key]');
-    console.log(button);
-    if (!button) {
-        return;
-    }
-    if (button.dataset.key=='creators'){
-        bottomBlock.classList.add('creators')
-    }else {
-        bottomBlock.classList.remove('creators')
-    }
-    showParticipants(participants[button.dataset.key])
+let bottomBlock = document.querySelector(".bottom");
+bottomBlock.addEventListener("click", (event) => {
+  let button = event.target.closest("[data-key]");
+  console.log(button);
+  if (!button) {
+    return;
+  }
+  if (button.dataset.key == "creators") {
+    bottomBlock.classList.add("creators");
+  } else {
+    bottomBlock.classList.remove("creators");
+  }
+  showParticipants(participants[button.dataset.key]);
+});
 
-    
+function toggleModal() {
+  const modal = document.querySelector(".my-modal");
+  modal.classList.toggle("show");
 }
-)
